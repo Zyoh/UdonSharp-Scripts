7a81d1ca972865ab1abe34346fd15d3bf4f460ef
Zoe <zyoh.32@gmail.com>
2020-04-09 02:27:16 -0400

    Add interact toggle

---
+++ InteractToggleGameObject.cs
@@ -0,0 +1,16 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ToggleObject : UdonSharpBehaviour
+{
+    [Tooltip("Put object here")]
+    public GameObject toggledObject;
+    
+    public override void Interact()
+    {
+        toggledObject.SetActive(!toggledObject.activeSelf);
+    }
+}
